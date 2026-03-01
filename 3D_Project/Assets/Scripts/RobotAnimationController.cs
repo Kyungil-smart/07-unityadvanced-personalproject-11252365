26a0d7a301de9ba201a74357e428bd7051831bf2
11252365 <tdkgus95111@gmail.com>
2026-03-01 23:55:54 +0900

    refactor : RobotAniController -> RobotAnimationController 변경

--- 3D_Project/Assets/Scripts/RobotAnimationController.cs
+++ 3D_Project/Assets/Scripts/RobotAnimationController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class RobotAniController : MonoBehaviour
+[DisallowMultipleComponent]
+public class RobotAnimationController : MonoBehaviour
 {
     private Animator _animator;
 
