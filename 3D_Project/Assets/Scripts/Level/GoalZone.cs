0d413a1ced918d8becce16b01e9cb2372308dcbb
11252365 <tdkgus95111@gmail.com>
2026-03-05 14:54:53 +0900

    feat : GoalZone 승리 판정 구현
    콜라이더의 트리거를 이용함

--- 3D_Project/Assets/Scripts/Level/GoalZone.cs
+++ 3D_Project/Assets/Scripts/Level/GoalZone.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class GoalZone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool _triggerOnce = true;
+    private bool _hasTriggered;
+
+    private void Reset()
     {
-        
+        GetComponent<Collider>().isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
+        if (_hasTriggered) return;
         
+        if (other.GetComponentInParent<RobotController>() == null
+            && other.GetComponentInParent<RobotCombatController>() == null)
+        {return;}
+        
+        _hasTriggered = true;
+        
+        if (_triggerOnce) GetComponent<Collider>().enabled = false;
+        
+        GameManager.Instance.Clear();
     }
 }
